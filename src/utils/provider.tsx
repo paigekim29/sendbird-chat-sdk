e5bd2aa610964c6ec17a0bdc7666f1fb3b00bd01
Paige <paigekim29@gmail.com>
2024-02-17 02:02:13 +0900

    Add dayjs library for date manipulation

--- src/utils/provider.tsx
+++ src/utils/provider.tsx
@@ -5,6 +5,15 @@ import enUS from 'antd-mobile/es/locales/en-US';
 import { Provider as JotaiProvider } from 'jotai';
 import { DevTools } from 'jotai-devtools';
 
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import utc from 'dayjs/plugin/utc';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+
+dayjs.extend(relativeTime);
+dayjs.extend(utc);
+dayjs.extend(localizedFormat);
+
 function Provider({ children }: React.PropsWithChildren) {
   return (
     <JotaiProvider>
