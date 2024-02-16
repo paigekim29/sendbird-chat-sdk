055c9a201353591b4bde822f635cd687675af9fb
Paige <paigekim29@gmail.com>
2024-02-16 21:58:12 +0900

    Add jotai provider

--- src/utils/provider.tsx
+++ src/utils/provider.tsx
@@ -2,14 +2,15 @@
 
 import { ConfigProvider } from 'antd-mobile';
 import enUS from 'antd-mobile/es/locales/en-US';
+import { Provider as JotaiProvider } from 'jotai';
 import { DevTools } from 'jotai-devtools';
 
 function Provider({ children }: React.PropsWithChildren) {
   return (
-    <>
+    <JotaiProvider>
       <ConfigProvider locale={enUS}>{children}</ConfigProvider>
       <DevTools />
-    </>
+    </JotaiProvider>
   );
 }
 
